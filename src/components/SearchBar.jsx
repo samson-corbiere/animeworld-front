eae190ab1477f9bb1f889977fbdee674ef5d2ec2
samson-corbiere <samson.corbiere@gmail.com>
2021-02-09 13:50:47 +0100

    construction searchbar is done

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,50 +1,62 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "./style/HomePageCard.css"
+import { Link } from "react-router-dom";
 
 function SearchBar() {
-  const [getResultsSearches, setGetResultsSearches] = useState([])
-  const [searches, setSearches] = useState("")
+  const [allAnime, setAllAnime] = useState([])
+  const [specificAnime, setSpecificAnime] = useState("")
 
-  const handleChangeNameAnime = (e) => {
-    setSearches(e.target.value);
-  }
-
-  const handleForm = async () => {
-    let params = {
-      searches,
-    };
-    try {
+  useEffect(() => {
+    const getListResults = async () => {
       await axios
-        .post(`/searches/nameAnime`, params)
-        .then((response) => response)
-        .then((response) => {
-          setGetResultsSearches(response);
-        });
-    } catch (err) {
-      alert("erreur recherche film");
+        .get(`http://localhost:5000/animes/all`)
+        .then((res) => {
+          setAllAnime(res.data);
+        })
     }
-  }
+    console.log(allAnime);
+    getListResults()
+  }, []);
 
+  const filteredAnimes = allAnime.filter((filteredAnimes) => {
+    return filteredAnimes.name
+      .toLowerCase()
+      .includes(specificAnime);
+  });
 
   return (
     <div>
-      <form onSubmit={handleForm}>
-      <input
-        className="champs"
-        type="text"
-        value={searches}
-        onChange={handleChangeNameAnime}
-      />
-      <input
-        type="submit"
-        value="chercher un anime"
-      />
-      </form>
       <div>
-        {getResultsSearches.name}
+        <input
+          type="text"
+          onChange={(e) => setSpecificAnime(e.target.value)}
+        />
+      </div>
+      <div className="HomePageAllCardAnime">
+        {filteredAnimes.map((anime) => (
+          <Link to={`/pageAnime/${anime.id}`}>
+            <div className="HomePageCardAnime">
+              <p>{anime.name}</p>
+              <img src={anime.image} className="HomePageImage" alt={anime.name}/>
+            </div>
+          </Link>
+        ))}
+        
       </div>
     </div>
   )
 }
 
 export default SearchBar;
+
+{/* <div className="HomePageAllCardAnime">
+  {listByView.map(anime => (
+    <Link to={`/pageAnime/${anime.id}`}>
+      <div className="HomePageCardAnime">
+        <p>{anime.name}</p>
+        <img src={anime.image} className="HomePageImage" alt={anime.name}/>
+      </div>
+    </Link>
+  ))}
+</div> */}
