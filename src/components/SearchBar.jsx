54a72b8e4e6d35cf1e86eafb39cf6c33233f8adf
samson-corbiere <samson.corbiere@gmail.com>
2021-02-09 13:58:20 +0100

    delete comment useless

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -49,14 +49,3 @@ function SearchBar() {
 }
 
 export default SearchBar;
-
-{/* <div className="HomePageAllCardAnime">
-  {listByView.map(anime => (
-    <Link to={`/pageAnime/${anime.id}`}>
-      <div className="HomePageCardAnime">
-        <p>{anime.name}</p>
-        <img src={anime.image} className="HomePageImage" alt={anime.name}/>
-      </div>
-    </Link>
-  ))}
-</div> */}
